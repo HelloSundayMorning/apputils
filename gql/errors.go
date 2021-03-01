39a018deee9c81e4bd426a4a6a89c98c9f995ddd
Yu Zhao <yu@hellosundaymorning.org>
2021-03-01 11:44:13 +1100

    Rename function and use context instead of path

--- gql/errors.go
+++ gql/errors.go
@@ -1,16 +1,25 @@
 package gql
 
 import (
+	"context"
+
+	"github.com/99designs/gqlgen/graphql"
 	"github.com/vektah/gqlparser/gqlerror"
 )
 
 // UnauthorizedErrorCode 401 indicates an unauthorized request
 const UnauthorizedErrorCode = 401
 
-// GetUnauthorizedError creates a new instance of Unauthorized error with the given path and message
-func GetUnauthorizedError(path *[]interface{}, message *string) *gqlerror.Error {
+// NewUnaothorizedError creates a new instance of Unauthorized error with the given context and message
+func NewUnaothorizedError(ctx context.Context, message *string) *gqlerror.Error {
+	paths := graphql.GetPath(ctx)
+	iPaths := make([]interface{}, len(paths))
+	for i, path := range paths {
+		iPaths[i] = path
+	}
+
 	return &gqlerror.Error{
-		Path:    *path,
+		Path:    iPaths,
 		Message: *message,
 		Extensions: map[string]interface{}{
 			"code": UnauthorizedErrorCode,
