fb06b82c13a7b4cf5d3c0c35e94cbad4e9c096bc
Gustavo Gambarini <gambarini@gmail.com>
2021-03-26 16:48:52 +1100

    added sql tracing, env setup and subsegment log tracing

--- app/app.go
+++ app/app.go
@@ -4,9 +4,11 @@ type ApplicationID string
 
 
 const (
-	AppVersionEnv = "APP_VERSION"
+	AppVersionEnv     = "APP_VERSION"
 	AppEnvironmentEnv = "APP_ENVIRONMENT"
+	AwsXrayDisableEnv = "AWS_XRAY_SDK_DISABLED"
 
 	StagingEnvironment = "staging"
 	ProductionEnvironment = "production"
+	LocalEnvironment = "local"
 )
