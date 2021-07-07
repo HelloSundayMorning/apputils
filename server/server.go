cbad896559610a44a02f89fc7e1632268cbe929e
Gustavo Gambarini <gambarini@gmail.com>
2021-07-07 10:01:14 +1000

    Tracing GQL Path (#9)
    * log
    
    * GQL tracing
    
    * GQL tracing
    
    * GQL tracing
    
    * GQL tracing

--- server/server.go
+++ server/server.go
@@ -204,13 +204,14 @@ func (srv *AppServer) AddGraphQLHandler(path string, gqlSchema graphql.Executabl
 
 	gqlServer := gqlHandlers.NewDefaultServer(gqlSchema)
 
+	gqlServer.AroundFields(func(ctx context.Context, next graphql.Resolver) (res interface{}, err error) {
+		tracing.AddTracingGraphQLInfo(ctx)
+		return next(ctx)
+	})
+
 	srv.router().HandleFunc(path, srv.requestInterceptor(func(writer http.ResponseWriter, request *http.Request) {
 
-		ctx := appctx.NewContext(request)
-
-		gqlServer.ServeHTTP(writer, request.WithContext(ctx))
-
-		tracing.AddTracingGraphQLInfo(request.Context())
+		gqlServer.ServeHTTP(writer, request)
 
 	})).Methods("POST")
 
@@ -478,7 +479,7 @@ func (srv *AppServer) requestInterceptor(next http.HandlerFunc) http.HandlerFunc
 		// Here wrapping request in a AWS XRay segment handler to trace the Request
 		xRayHandler := srv.newXraySegmentHandler(next)
 
-		xRayHandler.ServeHTTP(w, r)
+		xRayHandler.ServeHTTP(w, r.WithContext(ctx))
 
 	}
 }
@@ -490,7 +491,7 @@ func (srv *AppServer) newXraySegmentHandler(next http.HandlerFunc) http.Handler 
 		tracing.AddCustomTracingWorkloadType(ctx, tracing.WorkloadTypeHTTPCall)
 		tracing.AddTracingAnnotationFromCtx(ctx)
 
-		next.ServeHTTP(w, r)
+		next.ServeHTTP(w, r.WithContext(ctx))
 	}))
 	return xRayHandler
 }
